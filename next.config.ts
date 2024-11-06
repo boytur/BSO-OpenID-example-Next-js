49e5c8f54acef96f8211e2e8ee4ba3917f1f9cd8
boytur <104257779+boytur@users.noreply.github.com>
2024-11-06 15:30:09 +0700

    🔧 chore[next.config.ts] Allow images from specified external domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "cdn.discordapp.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
 };
 
 export default nextConfig;
